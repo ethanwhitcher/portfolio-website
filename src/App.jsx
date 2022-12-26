ed8726957c0c02c0a17678c8702a6e7748ff60af
ethanwhitcher <ewhitcher123@gmail.com>
2022-12-26 00:17:59 -0500

    added gotham fonts

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,13 @@
-import { useState } from 'react'
 import './App.css'
+import './fonts/Gotham-Font/GothamBold.ttf';
 
 function App() {
 
   return (
     <div className="App">
-
+      <div className='font-face1'>
+        <br></br>Designer
+      </div>
     </div>
   )
 }
